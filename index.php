82df24a6158b8877d27490753f730ae02aff8455
snarksliveshere <snarksliveshere@gmail.com>
2018-01-07 19:56:18 +0300

    ob_start View -> render() и применения

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ $user = new \App\Models\User();
 $view->users = \App\Models\User::findAll();
 $content = $view->render(__DIR__.'/App/templates/index.php');
 // вот примерно за этим
+// можно сделать специальные теги вставить, а потом - сделать визуальный редактор - ckeditor к примеру
+// или когда страницу сайтов собираем из нескольких кусочков - для каждого кусочка отдельный объект view делает свою работу а потом собирается
 echo str_replace('@','!!!',$content);
 
 
