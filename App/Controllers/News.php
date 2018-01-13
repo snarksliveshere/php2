6a8abc959ccb3e2604ef65f050f590d0d8b36b24
snarksliveshere <snarksliveshere@gmail.com>
2018-01-13 12:29:14 +0300

    + контроллер

---
+++ App/Controllers/News.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+use App\View;
+
+class News
+{
+    protected $view;
+    public function __construct()
+    {
+        $this->view = new \App\View();
+    }
+    public function action($action)
+    {
+        $methodName = 'action'.$action;
+        //можно что-то сделать перед вызовом
+        $this->beforeAction();
+        // по логике здесь идет обращение к самое переменной $methodName
+        return $this->$methodName();
+    }
+    protected function beforeAction()
+    {
+       echo 'COUNTER';
+    }
+    protected function actionIndex()
+    {
+
+        $this->view->title = 'Lazy Load';
+        $this->view->news = \App\Models\News::findAll();
+        $this->view->display(__DIR__.'/../templates/news.php');
+    }
+}
--- index.php
+++ index.php
@@ -1,31 +1,8 @@
 <?php
 require __DIR__.'/autoload.php';
 
-$view = new \App\View();
-$view->title = 'Simple template';
-$user = new \App\Models\User();
-$view->users = \App\Models\User::findAll();
-$content = $view->render(__DIR__.'/App/templates/index.php');
-// вот примерно за этим
-// можно сделать специальные теги вставить, а потом - сделать визуальный редактор - ckeditor к примеру
-// или когда страницу сайтов собираем из нескольких кусочков - для каждого кусочка отдельный объект view делает свою работу а потом собирается
-echo str_replace('@','!!!',$content);
-$view->news = \App\Models\News::findAll();
-$content_news = $view->render(__DIR__.'/App/templates/news.php');
-echo $content_news;
-
-
-
-// CRUD
-//$users = \App\Models\User::findAll();
-//$users = \App\Models\User::findById(2);
-//$news = \App\Models\News::findAll();
-//include __DIR__.'/App/Views/news.php';
-//
-//$user = new \App\Models\User();
-//$user->name = 'Johns';
-//$user->email = 'johndos@doe.eu';
-//$user->update(3);
+$controller = new \App\Controllers\News();
+$controller->action('Index');
 
 
 
